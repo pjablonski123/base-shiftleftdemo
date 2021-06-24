d28b9d3a382d6c8e686465c4517c65734d5dd597
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2021-06-24 12:20:36 +0200

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -5,9 +5,9 @@ sleep 5
 #replace <buildname> with your buildname of your demo-build.yml file.
 REPOOUTPUT=$(curl -k -u $TL_USER:$TL_PASS \
   -H 'Content-Type: application/json' \
-  https://$TL_CONSOLE/api/v1/coderepos?id=pjablonski123%2Fbase-evil.petclinic&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
+  https://$TL_CONSOLE/api/v1/coderepos?id=pjablonski123%2Fdw-matrix&limit=15&offset=0&project=Central+Console&reverse=true&sort=vulnerabilityRiskScore)
  
-
+#base-evil.petclinic
 #sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d: > output.txt
 VULN=$(echo $REPOOUTPUT | sed -n -e 's/^.*\(vulnerabilitiesCount\)/\1/p' | cut -f1 -d, | cut -f2- -d:)
 #VULN=$(cat output.txt)
