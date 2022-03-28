0f4c62e7ce5469f76f6aea52011fe7ecd9a04920
pjablonski123 <36215334+pjablonski123@users.noreply.github.com>
2022-03-28 03:15:51 +0200

    Update checkGit.sh

--- files/checkGit.sh
+++ files/checkGit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-./twistcli coderepo scan --address https://$TL_CONSOLE --details -u $PC_USER -p $PC_PASS ./app 
-result=$(curl -k -u $PC_USER:$PC_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
+./twistcli coderepo scan --address https://$TL_CONSOLE --details -u $TL_USER -p $TL_PASS ./app 
+result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/coderepos-ci?limit=1&reverse=true&sort=scanTime"|jq '.[0].pass')
 
 if [ "$result" == "true" ]; then
    echo "Code Repo scan passed!"
